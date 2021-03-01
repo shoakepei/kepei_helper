1425a109ee8e098e3593bb3c872ead236e9091f4
shaokepei <shaokepei@foxmail.com>
2021-03-01 18:06:29 +0800

    1

--- src/WXBizDataCrypt.php
+++ src/WXBizDataCrypt.php
@@ -63,7 +63,7 @@ class WXBizDataCrypt
 			return \kepei_helper\ErrorCode::$IllegalBuffer;
 		}
 		$data = $result;
-		return \kepei_helper\ErrorCode::$OK;1
+		return \kepei_helper\ErrorCode::$OK;
 	}
 
 }
